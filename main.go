a64e521fcd832f6084277d0acaef6bfacad6ec11
JanarthananSMJ <janarthanansmj@gmail.com>
2025-10-15 10:57:04 +0530

    Bootstrap Peers Added

--- main.go
+++ main.go
@@ -12,9 +12,17 @@ import (
 )
 
 type Peer struct {
-	Id   peer.ID
-	Addr []multiaddr.Multiaddr
-	Host host.Host
+	Id        peer.ID
+	Addr      []multiaddr.Multiaddr
+	Host      host.Host
+	Bootstrap []peer.AddrInfo
+}
+
+var BootstrapAddresses = []string{
+	"/ip4/147.75.83.83/tcp/4001/p2p/QmNnooDu7fKXGG5kZsXK2xhmijT2v6d7HJv9Vid8yymvCj",
+	"/ip6/2604:1380:1000:6000::1/tcp/4001/p2p/QmNnooDu7fKXGG5kZsXK2xhmijT2v6d7HJv9Vid8yymvCj",
+	"/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ",
+	"/ip6/2604:a880:1:20::203:d001/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ",
 }
 
 func CreatePeer() (*Peer, error) {
@@ -25,10 +33,24 @@ func CreatePeer() (*Peer, error) {
 	host, err := libp2p.New(libp2p.Identity(privkey),
 		libp2p.ListenAddrStrings("/ip6/::/tcp/0", "/ip4/0.0.0.0/tcp/0"))
 
+	var bootstraps []peer.AddrInfo
+	for _, addrStr := range BootstrapAddresses {
+		maddr, err := multiaddr.NewMultiaddr(addrStr)
+		if err != nil {
+			fmt.Println("Error on Bootstrap Address : ", err)
+		}
+		info, err := peer.AddrInfoFromP2pAddr(maddr)
+		if err != nil {
+			fmt.Println("Failed to parse bootstrap info:", err)
+			continue
+		}
+		bootstraps = append(bootstraps, *info)
+	}
 	return &Peer{
-		Id:   host.ID(),
-		Addr: host.Addrs(),
-		Host: host,
+		Id:        host.ID(),
+		Addr:      host.Addrs(),
+		Host:      host,
+		Bootstrap: bootstraps,
 	}, nil
 }
 
@@ -44,5 +66,12 @@ func main() {
 	fmt.Println("Peer ID : ", mypeer.Id)
 	for _, addr := range mypeer.Addr {
 		fmt.Printf("Peer MultiAddress : %s/p2p/%s\n", addr.String(), mypeer.Id)
+	}
+
+	fmt.Println("\t\tBootstrap Peers\t\t")
+	for _, b := range mypeer.Bootstrap {
+		for _, a := range b.Addrs {
+			fmt.Printf("Bootstrap : %s/p2p/%s\n", a, b.ID)
+		}
 	}
 }
